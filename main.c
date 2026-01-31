fe3c78b5cfb0d591f27a8ea65bf59b2130169113
Raman Gosse <gosse.shelter@gmail.com>
2026-01-31 15:40:16 +0100

    Optimize

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
 //#include <stdlib.h>
@@ -8,33 +7,10 @@
 
 uint16_t s_grid[20];
 uint16_t *p_grid[20];
-int score = 0;
-//int score = 0;
-//uint16_t nextBlock = 0xACE1;
-
-/*
-    15 bit is 
-*/
-const uint8_t NEW_BLOCK = 0x1;
-
-uint8_t coords[8]; //x, y
-
-uint8_t flag = 0x20;
-/*
-    7 
-    6 
-    5 END_GAME
-    4 COLISION  
-    3 
-    2 
-    1 
-    0
-*/
-
 const uint8_t COLISION_MASK  = 0x10;
 const uint8_t GAME_LOOP_MASK = 0x20;
 
-const uint8_t figures[7][8] = {
+static const uint8_t figures[7][8] = {
     {4,1,4,0,3,1,5,1},
     {4,0,5,0,3,0,6,0}, 
     {4,0,4,1,5,0,5,1},  
@@ -43,18 +19,17 @@ const uint8_t figures[7][8] = {
     {4,1,4,0,4,2,5,0},
     {4,1,4,0,4,2,3,2}
 };
-
+const uint8_t BLOCK = '#';
+const uint8_t EMPTY = '.';
 
 void show(){
-  const uint8_t BLOCK = '#';
-  const uint8_t EMPTY = '.';
+
   uint16_t l = 1;
   uint8_t cursor = 0;
   char screenBuf[256];
   screenBuf[cursor++] = '\033';
   screenBuf[cursor++] = '[';
   screenBuf[cursor++] = 'H';
-  // printf("\033[H\n");
   for (uint8_t i = 0, j = 0; i < 20; i++) {
     for (j = 0; j < 10; j++) {
       screenBuf[cursor++] = (*p_grid[i]) & l ? BLOCK : EMPTY;
@@ -63,32 +38,19 @@ void show(){
     screenBuf[cursor++] = '\n';
     l = 1;
   }
-    write(1, screenBuf, cursor);
-
+  write(1, screenBuf, cursor);
 }
-
-void printBlock(uint8_t *x, uint8_t *y){
-    *p_grid[*y]|=1<<*x;
-}
-
 
 void printTetris(uint8_t *fig){
-    printBlock(&fig[0], &fig[1]);
-    printBlock(&fig[2], &fig[3]);
-    printBlock(&fig[4], &fig[5]);
-    printBlock(&fig[6], &fig[7]);
-}
-
-
-void clearBlock(uint8_t *x, uint8_t *y){
-    *p_grid[*y]&=~(1<<*x);
+    for(uint8_t i = 0; i < 8; i+=2){
+        *p_grid[fig[i + 1]]|=1<<fig[i];
+    }
 }
 
 void clearTetris(uint8_t *fig){
-    clearBlock(&fig[0], &fig[1]);
-    clearBlock(&fig[2], &fig[3]);
-    clearBlock(&fig[4], &fig[5]);
-    clearBlock(&fig[6], &fig[7]);
+    for(uint8_t i = 0; i < 8; i+=2){
+        *p_grid[fig[i + 1]]&=~(1<<fig[i]);
+    }
 }
 
 void tetrisDown(uint8_t *fig, uint8_t *f){
@@ -103,29 +65,29 @@ void tetrisDown(uint8_t *fig, uint8_t *f){
 }
 
 
-void setCoords(uint16_t *t){
+void setCoords(uint8_t *coords,uint16_t *t, uint8_t *flag){
     if((*p_grid[0]) != 0) {
-        flag&=~(GAME_LOOP_MASK);
+        *flag&=~(GAME_LOOP_MASK);
         return;
     }
     for(int i = 0; i < 8; i++){
         coords[i] = figures[*t % 7][i];
     }
-    *t^= *t  >> 7;
+    *t^= *t >> 7;
     *t^= *t << 9;
     *t^= *t >>13;
 
 }
 
-void checkColision(uint16_t *nextBlock){
-    if((flag & COLISION_MASK) == COLISION_MASK){
+void checkColision(uint8_t *coords, uint16_t *nextBlock, uint8_t *flag){
+    if((*flag & COLISION_MASK) == COLISION_MASK){
         printTetris(coords);
-        setCoords(nextBlock);
-        flag^=COLISION_MASK;
+        setCoords(coords, nextBlock, flag);
+        *flag^=COLISION_MASK;
     }
 }
 
-void rotateTetris(){
+void rotateTetris(uint8_t *coords){
     int8_t buf[8];
     for(uint8_t i =0; i < 8;i+=2){
         buf[i] = coords[1] + coords[0] - coords[i + 1];
@@ -141,7 +103,7 @@ void rotateTetris(){
     }
 }
 
-void horMoveLeft(){
+void horMoveLeft(uint8_t *coords){
     if(!(coords[0] > 0) || (*p_grid[coords[1]] & (1 << (coords[0] - 1)))) return;
     if(!(coords[2] > 0) || (*p_grid[coords[3]] & (1 << (coords[2] - 1)))) return;
     if(!(coords[4] > 0) || (*p_grid[coords[5]] & (1 << (coords[4] - 1)))) return;
@@ -153,7 +115,7 @@ void horMoveLeft(){
 }
 
 
-void horMoveRight(){
+void horMoveRight(uint8_t *coords){
     if(!(coords[0] < 9) || (*p_grid[coords[1]] & (1 << (coords[0] + 1)))) return;
     if(!(coords[2] < 9) || (*p_grid[coords[3]] & (1 << (coords[2] + 1)))) return;
     if(!(coords[4] < 9) || (*p_grid[coords[5]] & (1 << (coords[4] + 1)))) return;
@@ -164,9 +126,10 @@ void horMoveRight(){
     coords[6]++;
 }
 
-void checkTetris(uint8_t *speed){
+void checkTetris(uint16_t *score, uint8_t *speed){
   uint16_t l = 0x3FF;
   uint16_t *b;
+  uint8_t screen[3];
   for(int i = 19; i >= 0; i--){
       if((*p_grid[i] & l) == l){
           b = p_grid[i];
@@ -175,11 +138,13 @@ void checkTetris(uint8_t *speed){
           }
           p_grid[0] = b;
           *p_grid[0] = 0;
-          score+= 100;
+          *score = *score + 1;
+            screen[0] = '0' + (*score / 100);
+            screen[1] = '0' + ((*score / 10) % 10);
+            screen[2] = '0' + (*score % 10);
+            write(1, &screen, 3);
       }
   }
-  printf("Score: %d", score);
-  *speed-=(score / 1000);
 }
 
 int main(void){
@@ -189,16 +154,19 @@ int main(void){
   }
   uint16_t nextBlock = 0xACE1;
   uint8_t speed = 20;
-  printf("Score: 0");
-  setCoords(&nextBlock);
+  uint8_t coords[8]; // x, y
+  uint8_t flag = 0x20;
   uint8_t tick = 0;
-  int c;
+  int8_t c;
+  int8_t input_size = 0;
   struct termios t, old;
+  uint16_t score = 0;
   tcgetattr(STDIN_FILENO, &t);
   old = t;
   t.c_lflag &= ~(ICANON | ECHO);
   tcsetattr(STDIN_FILENO, 0, &t);
   fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+  setCoords(coords, &nextBlock, &flag);
   do {
     printTetris(coords);
     show();
@@ -206,39 +174,19 @@ int main(void){
       clearTetris(coords);
       tetrisDown(coords, &flag);
       tick = 0;
-      printf("Speed: %d", speed);
     }
-    c = getchar();
-    if (c != EOF) {
+    input_size = read(0, &c, 1);
+    if (input_size > 0) {
       clearTetris(coords);
-      if (c == 'a') {
-        horMoveLeft();
-      }
-      if (c == 'd') {
-        horMoveRight();
-      }
-      if (c == 'w') {
-        rotateTetris();
-      }
+      if (c == 'a') horMoveLeft(coords);
+      if (c == 'd') horMoveRight(coords);
+      if (c == 'w') rotateTetris(coords);
     }
-    // printTetris(coords);
-    // show();
     clearTetris(coords);
-    checkColision(&nextBlock);
-    checkTetris(&speed);
+    checkColision(coords, &nextBlock, &flag);
+    checkTetris(&score, &speed);
     usleep(10000);
     tick++;
   }while (flag & GAME_LOOP_MASK);
   tcsetattr(STDIN_FILENO, TCSANOW, &old); // restore old settings
 }
-
-// printTetris(coords);
-// show();
-// pthread_mutex_unlock(&mtx);
-// usleep(500000);
-// pthread_mutex_lock(&mtx);
-// printTetris(coords);
-// clearTetris(coords);
-// tetrisDown(coords, &flag);
-// checkColision();
-// checkTetris();
